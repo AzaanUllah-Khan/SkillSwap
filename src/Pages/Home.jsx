afab6880883a209060a7721fffdecbc61d794bb4
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2025-07-26 21:52:16 +0500

    if function restored

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -37,13 +37,13 @@ const Home = () => {
         document.title = "SkillSwap | Teach what you know. Learn what you want.";
     }, []);
 
-    // if (loading) {
-        //     return (
-            //         <div className="absolute inset-0 flex items-center justify-center z-50">
-            //             <div className="w-12 h-12 border-4 border-indigo-600 border-t-transparent rounded-full animate-spin"></div>
-    //         </div>
-    //     );
-    // }
+    if (loading) {
+            return (
+                    <div className="absolute inset-0 flex items-center justify-center z-50">
+                        <div className="w-12 h-12 border-4 border-indigo-600 border-t-transparent rounded-full animate-spin"></div>
+            </div>
+        );
+    }
 
     const type = [
         {
