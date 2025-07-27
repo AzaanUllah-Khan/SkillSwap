7203722bf40163fdc6c7887643a6fc1dde80b6ba
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2025-07-27 23:24:28 +0500

    popup card Ui corrected

--- src/Pages/SeeProfile.jsx
+++ src/Pages/SeeProfile.jsx
@@ -192,7 +192,7 @@ const SeeProfile = () => {
                                 <button disabled={loading}
                                     type="button"
                                     onClick={() => editThis(editUid)}
-                                    className={loading ? "cursor-not-allowed flex justify-center rounded-md bg-indigo-400 px-3 py-2 text-sm/6 font-semibold text-white shadow-xs" : "cursor-pointer flex justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"}
+                                    className={loading ? "w-full cursor-not-allowed flex justify-center rounded-md bg-indigo-400 px-3 py-2 text-sm/6 font-semibold text-white shadow-xs sm:w-auto" : "w-full cursor-pointer flex justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:w-auto"}
                                 >
                                     {loading ? <svg className="size-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg> : 'Save Changes'}
                                 </button>
